4995f140f8094721ad5d08bc01ddff65d33123af
KUWASHIMA Yuichiro <kuwashima@upplan.jp>
2013-01-18 09:35:06 +0900

    initial

---
+++ lemon_parser.h
@@ -0,0 +1,7 @@
+//--------------------------------------------------------- ./lemon_parser.h
+
+#ifndef _LEMON_PARSER_H
+# define _LEMON_PARSER_H
+# include "scanner.h"
+scanner_token parse_math_expr(char* s);
+#endif
